b57ade1d9946c2d6839fed8ccc7b2f40d85cd555
Rithul Kamesh <hi@rithul.dev>
2024-09-27 22:53:36 +0530

    feat: update bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Projects from "@/components/projects";
+import Link from "@/components/link";
 
 const Home: React.FC = () => {
   const age = Math.floor(
@@ -12,18 +13,29 @@ const Home: React.FC = () => {
   return (
     <div className={`mx-auto max-w-container px-4 flex flex-col gap-3 py-8`}>
       <Header />
-      <div className="flex flex-col gap-1 mt-4">
+      <div className="flex flex-col gap-2 mt-4 mb-6">
         <p>
-          Hey There! I&apos;m Rithul, {age} and going to university. I
-          specialize in bringing ideas from imagination to reality with a focus
-          on practical application.
+          Hey There! I&apos;m Rithul, {age} and going to university. I am a
+          tech-enthusiast who has a research interest in AI/ML and Control
+          Systems. Coming from web development, I&apos;ve picked up an interest
+          for low-level programming with Rust and Zig, and I&apos;m trying to
+          improve my skill there.
         </p>
         <p>
-          Besides Programming, I enjoy reading philosophy, music, art,
-          photography, fiction writing and gaming. I also spend my free time
-          learning new spoken and programming languages.
+          I&apos;m a big fan of gaming and I&apos;m currently playing
+          &quot;Ghost of Tsushima&quot; and &quot;Elden Ring&quot;. I also write
+          fiction in my free time and I run a developer community called{" "}
+          <Link href="://pragmahq.com">pragma</Link> where I&apos;m most active.
+        </p>
+        <p>
+          I&apos;m an avid lifter with frequent workout videos on my{" "}
+          <Link href="://instagram.com/rithulkamesh" target="_blank">
+            instagram
+          </Link>
+          . I also play the piano, and I&apos;m learning to play the guitar.
         </p>
       </div>
+      <Projects />
       <Footer />
     </div>
   );
