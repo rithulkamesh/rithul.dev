e46388ebff9408fdbc5117fb384a77d969cd4465
Rithul Kamesh <hi@rithul.dev>
2025-06-20 16:00:46 +0530

    add slides redirect
    Signed-off-by: Rithul Kamesh <hi@rithul.dev>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+ async redirects() {
+    return [
+      {
+        source: '/ewX9On',
+        destination: 'https://docs.google.com/presentation/d/1ikxgRezHBs18AY1z9lDwUmd_2aXWaKQ-/edit?usp=sharing&ouid=106419837811012127385&rtpof=true&sd=true',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
